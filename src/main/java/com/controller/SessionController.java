947b51f46075de3a7c67f0901560234cca4e3ac4
Tejas Shah <tejasshah2k19@gmail.com>
2024-04-20 19:58:14 +0530

    bean

--- src/main/java/com/controller/SessionController.java
+++ src/main/java/com/controller/SessionController.java
@@ -2,22 +2,35 @@ package com.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.bean.UserBean;
 
 @Controller
 public class SessionController {
 
 	@GetMapping("/signup")
-	public String  signup() {
-		return "Signup";//jsp name 
+	public String signup() {
+		return "Signup";// jsp name
 	}
-	
+
 	@GetMapping("/login")
 	public String login() {
-		return "Login"; 
+		return "Login";
 	}
 
 	@GetMapping("/")
 	public String welcome() {
 		return "Welcome";
+	}
+
+	@PostMapping("/saveuser")
+	public String saveUser(UserBean user) {
+		// request.getParameter()
+		// bean
+		System.out.println(user.getFirstName());
+		System.out.println(user.getEmail());
+		System.out.println(user.getPassword());
+		return "Login";
 	}
 }
--- src/main/webapp/WEB-INF/views/Signup.jsp
+++ src/main/webapp/WEB-INF/views/Signup.jsp
@@ -10,6 +10,14 @@
 <h2>Signup</h2>
 
 
+<form action="saveuser" method="post">
+	FirstName: <input type="text" name="firstName"/><bR><Br> 
+	Email:<input type="text" name="email"/><bR><Br> 
+	Password:<input type="text" name="password"/><bR><Br> 
+	
+	<input type="submit" value="Signup"/> 
+</form>
+
 <a href="login">Login</a>
 </body>
 </html>
